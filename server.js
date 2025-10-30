ea3354a7f3ea61fd261443317d05de7d66fe0fac
danielmoellerberg <danielmberg8@gmail.com>
2025-10-30 13:18:16 +0100

    server.js
    Tilføjet nyt i server.js og ligeledes stellet public/server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const PORT = 3000;
 
-// Gør mappen 'public' tilgængelig
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
-// Start serveren
+// Servér statiske filer fra "public"-mappen
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/status", (req, res) => {
+  res.send(`Server kører på port ${PORT}`);
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Serveren kører på http://161.35.76.75:${PORT}`);
 });
